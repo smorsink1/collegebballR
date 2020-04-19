c3d6500528396b69843f88ac54b51eb8356e9451
Silas Jack Morsink <silas@pandopooling.com>
2020-04-18 20:38:06 -0400

    Git-initializing commit

---
+++ R/extend_ncaa_pbp.R
@@ -0,0 +1,41 @@
+
+#' Add Team Info
+#'
+#' Adds team information to scraped play-by-play dataframe
+#' 
+#' @param pbp_df the raw play-by-play dataframe, ie the output of getGamePbp
+#'   (or scrapeSchoolSeason, scrapeConferenceSeason)
+#' 
+#' @return returns a 17-column data frame, adding columns for year, away (visiting team), 
+#'   home (home team), away_conference (ie Pac-12), away_division (ie 1 for Division 1), 
+#'   home_conference, and home_division
+#'
+#' @importFrom dplyr left_join rename select
+#' @importFrom magrittr %>%
+#' 
+#' @examples 
+#' game_pbp <- getGamePbp("https://stats.ncaa.org/game/index/4502166?org_id=674")
+#' addTeamInfo(game_pbp)
+#' \dontrun{
+#'   season_pbp <- scrapeSchoolSeason("Stanford", 2018)
+#'   addteamInfo(season_pbp)
+#' }
+#' 
+addTeamInfo <- function(pbp_df) {
+  pbp_df[["year"]] <- substr(pbp_df$date, nchar(pbp_df$date) - 3, nchar(pbp_df$date)) %>%
+    as.integer()
+  pbp_df[["away"]] <- pbp_df$batting[pbp_df$inning_top_bot == "top"][1]
+  pbp_df[["home"]] <- pbp_df$batting[pbp_df$inning_top_bot == "bot"][1]
+  away_school_info <- getSchoolMap() %>%
+    dplyr::select(school, conference, division, year) %>%
+    dplyr::rename("away" = "school", "away_conference" = "conference",
+                  "away_division" = "division") 
+  home_school_info <- getSchoolMap() %>%
+    dplyr::select(school, conference, division, year) %>%
+    dplyr::rename("home" = "school", "home_conference" = "conference",
+                  "home_division" = "division")
+  pbp_df_upd <- pbp_df %>%
+    dplyr::left_join(away_school_info, by = c("year", "away")) %>%
+    dplyr::left_join(home_school_info, by = c("year", "home"))
+  return (pbp_df_upd)
+}
---
+++ R/scrape_ncaa_pbp.R
@@ -0,0 +1,135 @@
+
+#' Get School Map
+#' 
+#' Obtain table mapping school names to conferences, 
+#'   school_id, year, division, conference_id
+#'   
+#' @return returns a 6-column data frame with columns for school, conference, school_id,
+#'   year, division, and conference_id
+#' 
+#' @importFrom baseballr school_id_lu
+#' 
+#' @examples 
+#' getSchoolMap()
+#' 
+#' @export
+getSchoolMap <- function() {
+  return (baseballr::school_id_lu(''))
+}
+
+#' Scrape Game Play-by-Play
+#' 
+#' Given a game_info_url, returns the play-by-play for that game
+#' 
+#' @param game_info_url the stats.ncaa.org page with the game play-by-play,
+#'   accessible via baseballr::get_ncaa_schedule_info function 
+#'   
+#' @return returns a 10-column data frame with columns for date, location, attendance,
+#'   inning, inning_top_bot, score, batting, fielding, description, game_info_url
+#' 
+#' @importFrom baseballr get_ncaa_baseball_pbp
+#' 
+#' @examples 
+#' getGamePbp("https://stats.ncaa.org/game/index/4502166?org_id=674")
+#' 
+#' @export
+getGamePbp <- function(game_info_url) {
+  game_pbp <- baseballr::get_ncaa_baseball_pbp(game_info_url)
+  game_pbp[["game_info_url"]] <- game_info_url
+  return (game_pbp)
+}
+
+#' Get School Season URLs
+#' 
+#' Obtain game_info_url values for all games in a school's season
+#' 
+#' @param school_name the name of the school 
+#'   (must have an exact match in school column of getSchoolMap()) 
+#' @param season_year the year of the season to acquire data for
+#'   (must have an exact match in the year column of getSchoolMap())
+#'   
+#' @return returns a character vector of URLs for each game
+#' 
+#' @importFrom baseballr get_ncaa_schedule_info
+#' 
+#' @examples 
+#' getSchoolSeasonURLs("Stanford", 2018)
+#' 
+#' @export
+#' 
+getSchoolSeasonURLs <- function(school_name, season_year) {
+  school_map <- getSchoolMap()
+  school_matches <- which(school_map$school == school_name)
+  year_matches <- which(school_map$year == season_year)
+  school_map_row <- school_map[intersect(school_matches, year_matches), ]
+  if (nrow(school_map_row) < 1) {
+    stop ("no records match that school_name / season_year combination")
+  } else if (nrow(school_map_row) > 1) {
+    school_map_row <- school_map_row[1, ]
+  }
+  season_info <- baseballr::get_ncaa_schedule_info(school_map_row$school_id, school_map_row$year)
+  season_urls <- season_info$game_info_url
+  return (season_urls)
+}
+
+#' Scrape School Season 
+#' 
+#' Obtain play-by-play data for all games in a school's season
+#' 
+#' @param school_name the name of the school 
+#'   (must have an exact match in school column of getSchoolMap()) 
+#' @param season_year the year of the season to acquire data for
+#'   (must have an exact match in the year column of getSchoolMap())
+#'   
+#' @return returns a 10-column data frame with columns for date, location, attendance,
+#'   inning, inning_top_bot, score, batting, fielding, description, and game_info_url
+#' 
+#' @importFrom baseballr get_ncaa_schedule_info 
+#' @importFrom purrr map_dfr
+#' 
+#' @examples
+#' \dontrun{scrapeSchoolSeason("Stanford", 2018)}
+#' 
+#' @export
+#'
+scrapeSchoolSeason <- function(school_name, season_year) {
+  season_urls <- getSchoolSeasonURLs(school_name, season_year)
+  season_pbp <- purrr::map_dfr(season_urls, getGamePbp)
+  return (season_pbp)
+}
+
+#' Scrape Conference Season 
+#' 
+#' Obtain play-by-play data for all games in a season played by all teams in a given conference
+#' 
+#' @param conference_name the name of the conference 
+#'   (must have an exact match in conference column of getSchoolMap()) 
+#' @param season_year the year of the season to acquire data for
+#'   (must have an exact match in the year column of getSchoolMap())
+#'   
+#' @return returns a 10-column data frame with columns for date, location, attendance,
+#'   inning, inning_top_bot, score, batting, fielding, description, and game_info_url
+#' 
+#' @importFrom baseballr get_ncaa_schedule_info 
+#' @importFrom purrr map_dfr
+#' 
+#' @examples
+#' \dontrun{scrapeConferenceSeason("Pac-12", 2018)}
+#' 
+#' @export
+#'
+scrapeConferenceSeason <- function(conference_name, season_year) {
+  school_map <- getSchoolMap()
+  conf_matches <- which(school_map$conference == conference_name)
+  year_matches <- which(school_map$year == season_year)
+  school_map_rows <- school_map[intersect(conf_matches, year_matches), ]
+  if (nrow(school_map_rows) < 1) {
+    stop ("no records match that conference_name / season_year combination")
+  }
+  all_urls <- unlist(purrr::map(school_map_rows$school, getSchoolSeasonURLs, season_year = season_year))
+  conf_urls <- all_urls[!duplicated(all_urls)]
+  conf_pbp <- purrr::map_dfr(conf_urls, getGamePbp) 
+  return (conf_pbp)
+}
+
+
