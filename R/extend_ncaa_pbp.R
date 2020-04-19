fcd74b0bf1fe4c6ce4c785f6a16ecc8325082a7d
Silas Jack Morsink <silas@pandopooling.com>
2020-04-18 20:47:09 -0400

    Added data folder with school_map

--- R/extend_ncaa_pbp.R
+++ R/extend_ncaa_pbp.R
@@ -2,25 +2,25 @@
 #' Add Team Info
 #'
 #' Adds team information to scraped play-by-play dataframe
-#' 
+#'
 #' @param pbp_df the raw play-by-play dataframe, ie the output of getGamePbp
 #'   (or scrapeSchoolSeason, scrapeConferenceSeason)
-#' 
-#' @return returns a 17-column data frame, adding columns for year, away (visiting team), 
-#'   home (home team), away_conference (ie Pac-12), away_division (ie 1 for Division 1), 
+#'
+#' @return returns a 17-column data frame, adding columns for year, away (visiting team),
+#'   home (home team), away_conference (ie Pac-12), away_division (ie 1 for Division 1),
 #'   home_conference, and home_division
 #'
 #' @importFrom dplyr left_join rename select
 #' @importFrom magrittr %>%
-#' 
-#' @examples 
+#'
+#' @examples
 #' game_pbp <- getGamePbp("https://stats.ncaa.org/game/index/4502166?org_id=674")
 #' addTeamInfo(game_pbp)
 #' \dontrun{
 #'   season_pbp <- scrapeSchoolSeason("Stanford", 2018)
 #'   addteamInfo(season_pbp)
 #' }
-#' 
+#'
 addTeamInfo <- function(pbp_df) {
   pbp_df[["year"]] <- substr(pbp_df$date, nchar(pbp_df$date) - 3, nchar(pbp_df$date)) %>%
     as.integer()
@@ -29,7 +29,7 @@ addTeamInfo <- function(pbp_df) {
   away_school_info <- getSchoolMap() %>%
     dplyr::select(school, conference, division, year) %>%
     dplyr::rename("away" = "school", "away_conference" = "conference",
-                  "away_division" = "division") 
+                  "away_division" = "division")
   home_school_info <- getSchoolMap() %>%
     dplyr::select(school, conference, division, year) %>%
     dplyr::rename("home" = "school", "home_conference" = "conference",
